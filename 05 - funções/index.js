94bc7804fbe1aa697d330725ac67f805f8f8780f
brie-dev <silva111gabi16@gmail.com>
2024-07-11 16:36:43 -0300

    Funções
    Exemplos de funções

---
+++ 05 - funções/index.js
@@ -0,0 +1,11 @@
+// PERFORMANDO UMA TASK
+function saudacao(nome, ultimoNome) { // (parametro)
+    console.log('Hello ' + nome + ' ' + ultimoNome);
+}
+saudacao('Gabrielle', 'Silva'); //(argumento)
+
+// CALCULANDO UM VALOR
+function conta(numero) {
+    return numero * numero;
+}
+console.log(conta(2));
