84bd4deb42a93e488a4df9389acf55d078724e9a
brie-dev <silva111gabi16@gmail.com>
2024-07-11 15:00:36 -0300

    Variaveis
    Var
    Let
    Const

---
+++ variaveis/index.js
@@ -0,0 +1,14 @@
+let nome = 'Gabrielle';
+console.log(nome);
+
+let firstName = 'Gabrielle'
+let lastName = 'Rodrigues'
+console.log(firstName, lastName) 
+
+const numeroAleatorio = 7
+numeroAleatorio = 3 //VAI DAR ERRO
+console.log(numeroAleatorio)
+
+// VAR NÃO É RECOMENDADO A USAR
+// CONST PARA NUMEROS, STRINGS DEFINIDOS,ETC QUE NÃO IRA MUDAR
+// LET PARA NUMEROS, STRINGS, ETC QUE PODEM MUDAR
