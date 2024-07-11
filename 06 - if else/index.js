608c1a419dae58477a116aa54c222557452a71c6
brie-dev <silva111gabi16@gmail.com>
2024-07-11 17:17:41 -0300

    if, else if, else

---
+++ 06 - if else/index.js
@@ -0,0 +1,11 @@
+let horas = 20;
+
+if (horas >= 6 && horas < 12) {
+    console.log('Bom dia');
+}
+else if (horas >= 12 && horas < 18) {
+    console.log('Boa tarde');
+}
+else {
+    console.log('Boa noite')
+}
