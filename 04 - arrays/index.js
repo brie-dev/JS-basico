aca330821c8295e038d93ab918e77037cc3bebd7
brie-dev <silva111gabi16@gmail.com>
2024-07-12 14:20:07 -0300

    Arrays (push, pop, map)

--- 04 - arrays/index.js
+++ 04 - arrays/index.js
@@ -8,7 +8,18 @@ console.log(coresSelecionadas[0])
 
 // PARA ADICIONAR MAIS UMA COR
 coresSelecionadas[2] = 'verde'
+// ou
+coresSelecionadas.push('amarelo')
 console.log(coresSelecionadas)
+
+// PARA REMOVER O ULTIMO ELEMENTO DO ARRAY E RETORNAR ESSE ELEMENTO
+let ultimoElemento = coresSelecionadas.pop()
+console.log(ultimoElemento)
+
+// CRIAR UM NOVO ARRAY COM OS RESULTADOS DE UMA FUNÇÃO PARA CADA ELEMENTO DO ARRAY ORIGINAL
+let numeros = [1, 2, 3];
+let duplicados = numeros.map(num => num * 2);
+console.log(duplicados)
 
 // VER ELEMENTOS
 console.log(coresSelecionadas.length)
